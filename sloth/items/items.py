7f2f8f5ccbe63d82edbf38b1dfe6f73e23af8958
Thuyen Ngo <vanthuyen@gmail.com>
2017-10-31 14:25:32 -0700

    make polygon movable

--- sloth/items/items.py
+++ sloth/items/items.py
@@ -767,6 +767,9 @@ class PolygonItem(BaseItem):
         #              QGraphicsItem.ItemSendsGeometryChanges |
         #              QGraphicsItem.ItemSendsScenePositionChanges)
         self._polygon = None
+        self._start_pos = None
+        self._start_polygon = None
+        self._move = False
 
         self._updatePolygon(self._dataToPolygon(self._model_item))
         LOG.debug("Constructed polygon %s for model item %s" %
@@ -830,3 +833,37 @@ class PolygonItem(BaseItem):
     def dataChange(self):
         polygon = self._dataToPolygon(self._model_item)
         self._updatePolygon(polygon)
+
+    def mousePressEvent(self, event):
+        if event.button():
+            self._start_pos = event.scenePos()
+            self._start_polygon = self._polygon
+            self._move = True
+        BaseItem.mousePressEvent(self, event)
+
+    def mouseMoveEvent(self, event):
+        if self._move:
+            diff = event.scenePos() - self._start_pos
+
+            polygon = QPolygonF()
+            for p in self._start_polygon:
+              polygon.append(p + diff)
+
+            xn = [str(p.x()) for p in polygon]
+            yn = [str(p.y()) for p in polygon]
+
+            xn = ';'.join(xn)
+            yn = ';'.join(yn)
+
+            self._updatePolygon(polygon)
+            self.updateModel({'xn':xn, 'yn':yn})
+            event.accept()
+        else:
+            BaseItem.mouseMoveEvent(self, event)
+
+    def mouseReleaseEvent(self, event):
+        if self._move:
+            self._move = False
+            self._start_pos = None
+            self._start_polygon = None
+        BaseItem.mouseReleaseEvent(self, event)
